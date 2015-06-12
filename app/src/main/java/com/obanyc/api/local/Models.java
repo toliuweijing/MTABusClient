ee68f7de5383df1b229270b0c71aef5856645154
Weijing Liu <waiging.lau@gmail.com>
2015-06-12 08:46:17 -0400

    checkpoint

--- app/src/main/java/com/obanyc/api/local/Models.java
+++ app/src/main/java/com/obanyc/api/local/Models.java
@@ -5,10 +5,26 @@ import android.os.Parcelable;
 
 import com.google.auto.value.AutoValue;
 
+import java.util.List;
+
 public class Models {
   @AutoValue
   abstract static class NearbyBusQuery {
+    abstract List<RouteStopDirection> routeStopDirections();
+
+    @AutoValue.Builder
+    abstract static class Builder {
+      abstract Builder routeStopDirections(RouteStopDirection a);
+      abstract Builder build();
+    }
+  }
+
+  @AutoValue
+  abstract static class RouteStopDirection {
 
   }
+
+  @AutoValue
+  abstract static class
 
 }

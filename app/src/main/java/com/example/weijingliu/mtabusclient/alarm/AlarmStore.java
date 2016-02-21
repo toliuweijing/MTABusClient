42d31fcaeab9d120f4b8151ba6a74e9729afd270
Weijing Liu <waiging.lau@gmail.com>
2016-02-21 18:31:31 -0500

    Clean up

--- app/src/main/java/com/example/weijingliu/mtabusclient/alarm/AlarmStore.java
+++ app/src/main/java/com/example/weijingliu/mtabusclient/alarm/AlarmStore.java
@@ -1,16 +1,9 @@
 package com.example.weijingliu.mtabusclient.alarm;
 
 import com.example.weijingliu.mtabusclient.alarm.Models.Alarm;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.Nullable;
-
-import autovalue.shaded.com.google.common.common.base.Function;
-import autovalue.shaded.com.google.common.common.collect.Lists;
 
 /**
  * Created by toliuweijing on 6/15/15.
@@ -35,16 +28,5 @@ public class AlarmStore {
 
   public List<Alarm> getAll() {
     return new ArrayList<>(mAlarms);
-  }
-
-  public List<Alarm> getAll(final Alarm.Type type) {
-    List<Alarm> result = new ArrayList<>(mAlarms);
-    Iterables.removeIf(result, new Predicate<Alarm>() {
-      @Override
-      public boolean apply(Alarm input) {
-        return input.type() != type;
-      }
-    });
-    return result;
   }
 }
